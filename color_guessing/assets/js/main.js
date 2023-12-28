1626552ff1de1a8ebd15988e323f4f224e9585e5
FaridTvK <farid@super-code.de>
2023-12-28 09:39:26 +0100

    color-guessing

--- color_guessing/assets/js/main.js
+++ color_guessing/assets/js/main.js
@@ -2,7 +2,7 @@ const colorArr = [];
 
 console.log("colorArr", colorArr);
 
-const randomCol = () => `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)} )`
+const randomCol = () => `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
 
 
 console.log(randomCol());
@@ -16,24 +16,37 @@ console.log("colorArr", colorArr);
 
 const output = colorArr[Math.floor(Math.random() * colorArr.length)]
 
-document.querySelector("#question").innerHTML = output;
 
-// console.log(output);
+console.log({output});
 
 let buttons = () => {
-    document.getElementById("bt1").style.backgroundColor = colorArr.pop()
+    document.querySelector("#question").innerHTML = output;
+
+    let mycolor1 = document.getElementById("bt1").style.background = colorArr.pop()
+    console.log("mycolor1", mycolor1);
+
     let mycolor2 = document.getElementById("bt2").style.backgroundColor = colorArr.pop()
-    console.log(mycolor2);
-    document.getElementById("bt3").style.backgroundColor = colorArr.pop()
-    document.getElementById("bt4").style.backgroundColor = colorArr.pop()
-    document.getElementById("bt5").style.backgroundColor = colorArr.pop()
+    console.log("mycolor2", mycolor2);
+
+    let mycolor3 = document.getElementById("bt3").style.background = colorArr.pop()
+    console.log("mycolor3", mycolor3);
+    let mycolor4 = document.getElementById("bt4").style.background = colorArr.pop()
+    console.log("mycolor4", mycolor4);
+
+
+    let mycolor5 = document.getElementById("bt5").style.background = colorArr.pop()
+    console.log("mycolor5", mycolor5);
 }
 
 
 let checkCol = (id) => {
     console.log(id);
-    let res = document.querySelector("#res")
-    res.innerHTML = document.getElementById(id).style.backgroundColor == output ? "Das Stimmt" : "leider ist falsch"
+    let res = document.getElementById("res")
+    console.log(document.getElementById(id).style.background);
+    res.innerHTML = document.getElementById(id).style.background == output 
+    ? "Das Stimmt" 
+    : "leider ist falsch";
+    console.log("outputInderFunc",output);
 
 }
 
